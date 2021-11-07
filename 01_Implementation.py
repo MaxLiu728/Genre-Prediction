7f9fa0a6e6717753d00017998ee61444b919294f
Max Liu <renjing.liu@mail.utoronto.ca>
2021-11-07 16:38:07 -0500

    Begin to process

--- 01_Implementation.py
+++ 01_Implementation.py
@@ -1,4 +1,22 @@
 # Have added Git
 
 import pandas as pd
+import pandas as pd
+import numpy as np
+from sklearn.dummy import DummyClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+import string
 
+df_1= pd.read_csv('data.csv')
+b= df_1['Lyric'].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+
+cv= CountVectorizer(stop_words=['a','you','to','me','12'], min_df=2)
+transform_temp= cv.fit_transform(b)
+
+transform_temp.toarray().shape
+cv.get_feature_names()
+
+df_output= pd.DataFrame(transform_temp.toarray(),columns= cv.get_feature_names())
+
+df_output
